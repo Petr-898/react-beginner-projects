d0a9688169ad5849394cd2a7ba1a01b4ac71d1a5
Peter <nataom67om@gmail.com>
2025-05-19 11:07:42 +0700

    err1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
-import { Users } from './components/Users';
+import { Users } from './components/Users/index';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const { Users, setUsers } = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.console.warn(err);
+        alert('Oshibka pri poluchenii polsovateley');
+      });
+  });
   return (
     <div className="App">
       <Users />
